c418730951ab830790908c1e58b166bbbbb488a2
Davi <davimargelo@hotmail.com>
2020-08-17 22:07:35 -0300

    first commit

---
+++ src/main/java/com/cdhi/projectivbackend/ProjectIvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cdhi.projectivbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIvBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdhi/projectivbackend/ProjectIvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdhi.projectivbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
